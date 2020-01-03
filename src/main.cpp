02d343baf7ac5a82bb034fbef2e7f4c9b82c7904
duzhendong <zhendong.du@uisee.com>
2020-01-03 16:40:38 +0800

    增加系统调用函数system()的用法

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void json_rd_test(void)
 {
-    string filename = "./test/test.json";
+    string filename = "./test/json_rd_test.json";
     ofstream ofs(filename);
     Json::Reader reader;
     Json::Value  obj;
@@ -39,9 +39,31 @@ void json_rd_test(void)
     
 }
 
+void system_cmd_test(void)
+{
+    string filename = "system_cmd_test.txt";
+    string filepath = "../test/";
+    string file     = filepath + filename;
+    ofstream ofs(file);
+    if(!ofs.is_open())
+    {
+        cout << "Open file:" << filename << " failed!" << endl;
+        return;
+    }
+    ofs << "hello world" << endl;
+    ofs.close();
+    
+    system("ls -l ../test/");
+    string cmd = "find " + filepath + " -type f -mtime -2 -exec rm {} \\;";   //查找 ./test/ 文件夹下修改时间为2天内的文件并删除
+    cout << cmd << endl;
+    system(cmd.c_str());
+    system("ls -l ../test/");
+}
+
 int main(int argc, char **argv)
 {
     cout << "This is project: LearnCpp." << endl;
-    json_rd_test();
+    //json_rd_test();
+    system_cmd_test();
     return 0;
 }
